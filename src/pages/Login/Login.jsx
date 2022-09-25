652fbcacadeb5ae18b2020546e5ca924573d005c
CHAS-JIN <762173213@qq.com>
2022-09-25 22:00:10 +0800

    登录表单验证

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -9,108 +9,46 @@ import styles from './Login.module.css'
 const Login = () => {
 	const navigate = useNavigate()
 	const [isReg, setIsReg] = useState(false)
+	const [form] = Form.useForm()
 
 	// 返回上一页
 	const back = () => {
 		navigate(-1)
 	}
 
-	const footer = type => {
-		if (type === 'login') {
-			return (
-				<div className={styles.footer}>
-					<Button
-						className={styles.button}
-						size="large"
-						fill="outline"
-						color="primary"
-						onClick={() => setIsReg(true)}
-					>
-						注册
-					</Button>
-					<Button
-						className={styles.button}
-						type="submit"
-						color="success"
-						size="large"
-					>
-						登录
-					</Button>
-				</div>
-			)
-		} else if (type === 'reg') {
-			return (
-				<div className={styles.footer}>
-					<Button
-						className={styles.button}
-						size="large"
-						fill="outline"
-						color="primary"
-						onClick={() => setIsReg(false)}
-					>
-						返回登录
-					</Button>
-					<Button
-						className={styles.button}
-						type="submit"
-						color="success"
-						size="large"
-					>
-						注册
-					</Button>
-				</div>
-			)
-		}
+	const footer = () => {
+		return (
+			<div className={styles.footer}>
+				<Button
+					className={styles.button}
+					size="large"
+					fill="outline"
+					color="primary"
+					onClick={() =>
+						setIsReg(pre => {
+							if (form.getFieldValue('passwordConfirm')) {
+								form.resetFields(['passwordConfirm'])
+							}
+							return !pre
+						})
+					}
+				>
+					{isReg ? '返回登录' : '去注册'}
+				</Button>
+				<Button
+					className={styles.button}
+					type="submit"
+					color="success"
+					size="large"
+					onClick={() => {
+						console.log(form.getFieldValue('passwordConfirm'))
+					}}
+				>
+					{!isReg ? '登录' : '注册'}
+				</Button>
+			</div>
+		)
 	}
-
-	const renderForm = () => {
-		if (!isReg) {
-			return (
-				<Form name="login" mode="card" footer={footer('login')}>
-					<Form.Item
-						name="username"
-						rules={[{ required: true, message: '用户名不能为空' }]}
-					>
-						<Input placeholder="请输入用户名" />
-					</Form.Item>
-
-					<Form.Item
-						name="password"
-						rules={[{ required: true, message: '密码不能为空' }]}
-					>
-						<Input placeholder="请输入密码" type="password" />
-					</Form.Item>
-				</Form>
-			)
-		} else {
-			return (
-				<Form name="reg" mode="card" footer={footer('reg')}>
-					<Form.Item
-						name="username"
-						rules={[{ required: true, message: '用户名不能为空' }]}
-					>
-						<Input placeholder="请输入用户名" />
-					</Form.Item>
-
-					<Form.Item
-						name="password"
-						rules={[{ required: true, message: '密码不能为空' }]}
-					>
-						<Input placeholder="请输入密码" type="password" />
-					</Form.Item>
-
-					<Form.Item
-						name="passwordConfirm"
-						rules={[{ required: true, message: '请再次输入密码' },{validator:{confirmPass}}]}
-					>
-						<Input placeholder="请再次输入密码" type="password" />
-					</Form.Item>
-				</Form>
-			)
-		}
-	}
-
-    const confirmPass = () => {}
 
 	return (
 		<Backdrop>
@@ -119,7 +57,61 @@ const Login = () => {
 					登录
 				</NavBar>
 				<div className={styles.wrapper}>
-					<div className={styles.main}>{renderForm()}</div>
+					<div className={styles.main}>
+						<Form form={form} mode="card" footer={footer()}>
+							<Form.Item
+								name="username"
+								rules={[
+									{
+										required: true,
+										message: '用户名不能为空！',
+										type: 'string',
+										whitespace: true,
+									},
+								]}
+							>
+								<Input placeholder="请输入用户名" />
+							</Form.Item>
+
+							<Form.Item
+								name="password"
+								rules={[
+									{
+										required: true,
+										message: '密码不能为空！',
+										type: 'string',
+										whitespace: true,
+									},
+								]}
+							>
+								<Input placeholder="请输入密码" type="password" />
+							</Form.Item>
+
+							{!isReg ? null : (
+								<Form.Item
+									name="passwordConfirm"
+									dependencies={['password']}
+									rules={[
+										{
+											required: true,
+											message: '两次密码不一致！',
+											type: 'string',
+											whitespace: true,
+											validator: (_, value) => {
+												console.log(value)
+												if (value === form.getFieldValue('password')) {
+													return Promise.resolve()
+												}
+												return Promise.reject()
+											},
+										},
+									]}
+								>
+									<Input placeholder="请再次输入密码" type="password" />
+								</Form.Item>
+							)}
+						</Form>
+					</div>
 				</div>
 			</div>
 		</Backdrop>
